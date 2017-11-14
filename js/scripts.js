3c59a5611f9698b3eb527712700b1de4dd397842
DevSplinter <danieldrzazga.94@gmail.com>
2017-11-14 14:55:29 +0100

    edit html

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,33 @@
 let canvas = document.querySelector('#game-box');
 let ctx = canvas.getContext('2d');
-
 let plane = document.querySelector('img');
 let canvasTop = canvas.offsetTop;
 let sth2 = false;
 
 
   canvas.addEventListener('click', function(e){
-      console.log('pozycja aa: ' + aa.xPosition + ', ' + aa.yPosition);
-        console.log('pozycja aa2: ' + aa2.xPosition + ', ' + aa2.yPosition);
-        console.log('pozycja aa3: ' + aa3.xPosition + ', ' + aa3.yPosition);
+//      console.log('pozycja aa: ' + aa.xPosition + ', ' + aa.yPosition);
+//        console.log('pozycja aa2: ' + aa2.xPosition + ', ' + aa2.yPosition);
+//        console.log('pozycja aa3: ' + aa3.xPosition + ', ' + aa3.yPosition);
     let rect = canvas.getBoundingClientRect();
     let x = e.clientX - rect.left;
     let y = e.clientY - rect.top;
 //    ctx.rect(80,100,32,32);
-    if (ctx.isPointInPath(x,y)){
-        
-    }
+//    if (ctx.isPointInPath(x,y)){
+//        console.log("trafiony");
+//    }
+      console.log("click:" + x);
+      console.log("object x: " + aa.xPosition);
+      console.log();
+      if(((aa.xPosition + 32) > x && x > aa.xPosition) && ((aa.yPosition + 32) > y && y > aa.yPosition)){
+          console.log("trafiony obiekt aa");
+      }
+       if(((aa2.xPosition + 32) > x && x > aa2.xPosition) && ((aa2.yPosition + 32) > y && y > aa.yPosition)){
+          console.log("trafiony obiekt aa2");
+      }
+      if(((aa3.xPosition + 32) > x && x > aa3.xPosition) && ((aa3.yPosition + 32) > y && y > aa.yPosition)){
+          console.log("trafiony obiekt aa3");
+      }
     
 });
 
@@ -27,7 +38,6 @@ class Enemy{
         this.xPosition = this.randPosition();
         this.drawPlane();
         this.yPosition = this.interval();
-        this.logConsole();
     }
     randPosition(){
         let x;
@@ -48,13 +58,7 @@ class Enemy{
             that.yPosition = y;
             that.drawPlane(x,y);
         },25);
-            
-        
-        
         return y;
-    }
-    logConsole(){
-        console.log(this.yPosition + ',' + this.xPosition);
     }
     
 }
@@ -62,5 +66,6 @@ let aa = new Enemy();
 let aa2 = new Enemy();
 let aa3 = new Enemy();
 
-//console.log(aa.xPosition + ', ' + aa3.xPosition)
+
+
     
