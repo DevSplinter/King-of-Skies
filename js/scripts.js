f505242ed8b86982562d89c4641a466ee9886b3f
DevSplinter <danieldrzazga.94@gmail.com>
2017-11-14 12:08:47 +0100

    problem solved

--- js/scripts.js
+++ js/scripts.js
@@ -5,62 +5,62 @@ let plane = document.querySelector('img');
 let canvasTop = canvas.offsetTop;
 let sth2 = false;
 
-     
-//function drawPlane(){
-//    zmienna = setInterval(function(){
-//        sth += 1;
-//    ctx.beginPath();
-//        ctx.clearRect(80, sth, 32,32);
-//    ctx.drawImage(plane, 80, sth);
-//    ctx.rect(80,sth,32,32);
-//    ctx.closePath();
-//    },30);
-//    
-//}
-canvas.addEventListener('click', function(e){
+
+  canvas.addEventListener('click', function(e){
+      console.log('pozycja aa: ' + aa.xPosition + ', ' + aa.yPosition);
+        console.log('pozycja aa2: ' + aa2.xPosition + ', ' + aa2.yPosition);
+        console.log('pozycja aa3: ' + aa3.xPosition + ', ' + aa3.yPosition);
     let rect = canvas.getBoundingClientRect();
-    x = e.clientX - rect.left;
-    y = e.clientY - rect.top;
+    let x = e.clientX - rect.left;
+    let y = e.clientY - rect.top;
 //    ctx.rect(80,100,32,32);
     if (ctx.isPointInPath(x,y)){
-        clearInterval(this.intervalPlane);
-        console.log('trafiony');
+        
     }
     
 });
-
-
 
 class Enemy{
     constructor(){
         this.life = 2;
         this.cash = 50;
-        this.intervalPlane;
         this.xPosition = this.randPosition();
         this.drawPlane();
-        
-    }
-    drawPlane(){
-        let intervalPlane = this.intervalPlane;
-        let x = this.xPosition;
-        let y = 0;
-        
-        intervalPlane = setInterval(function(){
-        y += 1;
-//        ctx.beginPath();
-        ctx.clearRect(x, 0, 32, y+32);
-        ctx.drawImage(plane, x, y);
-        ctx.rect(x, y, 32, 32);
-        ctx.closePath();
-        },150);
+        this.yPosition = this.interval();
+        this.logConsole();
     }
     randPosition(){
-       return Math.floor(Math.random()*500);
+        let x;
+       return x = Math.floor(Math.random()*300);
     }
+    drawPlane(x,y){        
+        ctx.beginPath();
+        ctx.clearRect(x-2, 0, 36, y+32);
+        ctx.drawImage(plane, x, y);
+        ctx.rect(x, y, 32, 32);
+        };
+    interval(){
+        let y = 0;
+        let x = this.xPosition;
+        let that = this;
+        setInterval(function(){
+            y += 1;
+            that.yPosition = y;
+            that.drawPlane(x,y);
+        },25);
+            
+        
+        
+        return y;
+    }
+    logConsole(){
+        console.log(this.yPosition + ',' + this.xPosition);
+    }
+    
 }
 let aa = new Enemy();
 let aa2 = new Enemy();
 let aa3 = new Enemy();
 
-console.log(aa.xPosition + ', ' + aa3.xPosition)
+//console.log(aa.xPosition + ', ' + aa3.xPosition)
     
